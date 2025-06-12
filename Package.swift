29fd0165bcb783822b0e724570a3a91449eb284a
Curt Pope <curtisspope@gmail.com>
2025-06-12 10:34:12 -0700

    init

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "FontPackage",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FontPackage"),
+            name: "FontPackage",
+            resources: [
+                .process("Resources")
+            ]),
         .testTarget(
             name: "FontPackageTests",
             dependencies: ["FontPackage"]
--- Sources/FontPackage/FontPackage.swift
+++ Sources/FontPackage/FontPackage.swift
@@ -1,2 +1,51 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+import CoreText
+
+public enum GaramondFont {
+        /// The name of the font as registered in the system
+    public static let name = "AppleGaramond"
+    
+        /// Font sizes that can be used with the Garamond font
+    public struct Size {
+        public static let headline: CGFloat = 92
+    }
+    
+        /// Ensures the font is registered only once
+    nonisolated(unsafe) private static var hasRegistered = false
+    
+        /// Register the font with the system
+    @discardableResult
+    public static func register() -> Bool {
+        guard !hasRegistered else { return true }
+        hasRegistered = true
+        
+        let bundle = Bundle.module
+        guard let fontURL = bundle.url(forResource: name, withExtension: "ttf") else {
+            return false
+        }
+        
+        var error: Unmanaged<CFError>?
+        let success = CTFontManagerRegisterFontsForURL(fontURL as CFURL, .process, &error)
+        
+        if !success, let error = error?.takeUnretainedValue() {
+
+        }
+        
+        return success
+    }
+}
+
+    // MARK: - Font Extension
+
+public extension Font {
+        /// Creates a Garamond font with the specified size
+    static func garamond(size: CGFloat) -> Font {
+        _ = GaramondFont.register()
+        return .custom(GaramondFont.name, size: size)
+    }
+    
+    
+    static var garamondHeadline: Font {
+        .garamond(size:  GaramondFont.Size.headline)
+    }
+}
